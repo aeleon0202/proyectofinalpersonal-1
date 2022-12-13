b7ff8a3841efafe38e0a395a642b60739a53e675
405 <405@academicos.cesde.edu.co>
2022-12-13 11:29:31 -0500

    se crea pagina adicional

--- src/AmpliarInfo/AmpliarInfo.js
+++ src/AmpliarInfo/AmpliarInfo.js
@@ -1,15 +1,44 @@
+import { Footer } from '../shared/Footer/Footer'
+import './AmpliarInfo.css'
 import { useLocation } from "react-router-dom"
 export function AmpliarInfo(){
 
     let datosrecibidos=useLocation()
-
-
     let producto=datosrecibidos.state.productoSeleccionado
     console.log(producto)
 
     return(
         <>
-            <h1>{producto.nombre}</h1>
+            <div className="container">
+
+                <div className="row my-5">
+                    <div className="col-12 col-md-5 ">
+                        <img src={producto.foto} alt="foto" className="img-fluid w-100"></img>
+                    </div>
+                    <div className="col-12 col-md-4 border p-3">
+                        <h2 className="fw-bold">{producto.nombre}</h2>
+                        <h1 className="mt-3 display-5">{producto.precio}<span className="descuento ms-3">20 % off</span></h1><br></br>
+                        <span className="badge text-bg-primary">Descuento del día</span><br></br>
+                        <p className='mt-3'>Hasta 48 cuotas</p>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/spotyanderl.appspot.com/o/visa%20(1).png?alt=media&token=af06cabc-3791-49dc-b02e-f83f48a5bced' alt='' className='img-fluid'></img>
+                        <img src='https://firebasestorage.googleapis.com/v0/b/spotyanderl.appspot.com/o/american-express%20(3)%20(1).png?alt=media&token=b6bb848f-8c0e-4a54-b10c-6544e9ce68d0' alt='' className='img-fluid ms-3'></img><br/><br/>
+                        <i className="bi bi-truck mt-5 text-success fs-3"><span className='ms-3 fs-6 fw-bold'>Envio gratis a nivel nacional</span></i>
+                        <p>Conoce los tiempos de envio y las formas de entrega</p>
+                        <i className="bi bi-arrow-clockwise mt-5 text-success fs-3"><span className='ms-3 fs-6 fw-bold'>Devolucion gratis</span></i>
+                        <p>30 días para reembolso</p>
+                        
+                    </div>
+                </div>
+                <div className='row mb-5'>
+                    <div className='col-6'>
+                        <h2>Descripcion</h2>
+                        <p>{producto.descripcion}</p>
+                    </div>
+                </div>
+            </div>
+
+            <Footer></Footer>
+            
         </>
     )
 
--- src/Mercancia/Mercancia.js
+++ src/Mercancia/Mercancia.js
@@ -26,18 +26,21 @@ export function Mercancia(){
         {
             nombre:"Camiseta original gira colombiana",
             foto:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/producto1.jpg?alt=media&token=6b01ba87-63ea-4fd5-a411-5bc7160623ef",
-            precio:40
+            precio:"$ 120.000",
+            descripcion:"Descripción Camiseta de manga corta, confeccionada con tejido en galga fina y acabado compactado.Cuello redondo con 4 capas, cubre costuras reforzado en cuello y hombros y costuras laterales.100% algodón peinado, punto liso, 180 g/m2"
 
         },
         { 
             nombre:"LP Original disco morning sessions",
             foto:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/producto2.jpg?alt=media&token=808ac1ff-fa31-4d76-886f-7f3f0befea97",
-            precio:90
+            precio:"$ 67.000",
+            descripcion:"Fluance RT80 LP clásico de vinilo de alta fidelidad con cartucho Audio Technica AT91, transmisión por correa, preamplificador incorporado, contrapeso ajustable, base de madera maciza - Negro piano"
         },
         {
             nombre:"Mascota INCUBUS",
             foto:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/producto4.jpg?alt=media&token=41afe26a-bab9-48b9-bb68-c28425d6e544",
-            precio:25
+            precio:"$ 85.000",
+            descripcion:"Diseño suave y cómodo, innovador, el modelado es encantador, práctico.Material o Componentes: Exterior: poliéster, elastano; relleno: 100% poliéster"
         }
     ]
 
@@ -65,6 +68,8 @@ export function Mercancia(){
                                                 detectarEvento(producto)
                                             }
                                         } >Ampliar</button>
+
+                                    
 
                                     </div>
 
